3f5c386081387c638e4744aa5cea70573133d489
Anthony Car <agcarat@gmail.com>
2016-10-31 22:59:26 -0400

    tcp source and dst determined

--- pcapsession.cc
+++ pcapsession.cc
@@ -6,6 +6,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netinet/if_ether.h> /* includes net/ethernet.h */
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
 
 void pcap_callback(u_char *useless, const struct pcap_pkthdr* pkthdr, const u_char *packet) {
     if(packet == NULL)
@@ -18,6 +20,7 @@ void pcap_callback(u_char *useless, const struct pcap_pkthdr* pkthdr, const u_ch
     /* lets start with the ether header... */
     eptr = (struct ether_header *) packet;
 
+
     /* Do a couple of checks to see what packet type we have..*/
     if (ntohs (eptr->ether_type) == ETHERTYPE_IP)
     {
@@ -25,12 +28,9 @@ void pcap_callback(u_char *useless, const struct pcap_pkthdr* pkthdr, const u_ch
                 ntohs(eptr->ether_type),
                 ntohs(eptr->ether_type));
 
-    }else  if (ntohs (eptr->ether_type) == ETHERTYPE_ARP)
-    {
-        printf("Ethernet type hex:%x dec:%d is an ARP packet\n",
-                ntohs(eptr->ether_type),
-                ntohs(eptr->ether_type));
-
+    }else  if (ntohs (eptr->ether_type) == ETHERTYPE_ARP) {
+        //printf("Ethernet type hex:%x dec:%d is an ARP packet\n", ntohs(eptr->ether_type), ntohs(eptr->ether_type));
+        return;
     }else {
     //    printf("Ethernet type %x not IP\n", ntohs(eptr->ether_type));
         //skip these for now
@@ -43,30 +43,19 @@ void pcap_callback(u_char *useless, const struct pcap_pkthdr* pkthdr, const u_ch
      *   bpf_u_int32;         lebgth this packet (off wire)
      *  }
      */
-    printf("Grabbed packet of length %d\n",pkthdr->len);
-    printf("Recieved at ..... %s\n",ctime((const time_t*)&pkthdr->ts.tv_sec));
-    printf("Ethernet address length is %d\n",ETHER_HDR_LEN);
+ //   printf("Grabbed packet of length %d\n",pkthdr->len);
+  //  printf("Recieved at ..... %s\n",ctime((const time_t*)&pkthdr->ts.tv_sec));
+   // printf("Ethernet address length is %d\n",ETHER_HDR_LEN);
 
+    struct ip *ipp =(struct ip *) (eptr +1);
+    if (ipp->ip_p ==IPPROTO_TCP ) {
+        struct tcphdr * tcpp = (struct tcphdr *) (((char *)ipp) + (ipp->ip_hl << 2 ));
 
-    u_char *ptr; /* printing out hardware header info */
-    /* copied from Steven's UNP */
-    ptr = eptr->ether_dhost;
-    int i = ETHER_ADDR_LEN;
-    printf(" Destination Address:  ");
-    do{
-        printf("%s%x",(i == ETHER_ADDR_LEN) ? " " : ":",*ptr++);
+        printf("ds=%s:%d->", inet_ntoa(ipp->ip_src), ntohs(tcpp->source));
+        printf("%s:%d\n", inet_ntoa(ipp->ip_dst), ntohs(tcpp->dest));
 
-    }while(--i>0);
-    printf("\n");
+    }
 
-    ptr = eptr->ether_shost;
-    i = ETHER_ADDR_LEN;
-    printf(" Source Address:  ");
-    do{
-        printf("%s%x",(i == ETHER_ADDR_LEN) ? " " : ":",*ptr++);
-
-    }while(--i>0);
-    printf("\n");
 
 }
 
